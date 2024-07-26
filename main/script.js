39bef42fe8b009142b17742629706ce0ca81317d
Mohammed Malleck <mohammedmalleck@gmail.com>
2024-07-26 15:25:33 +0400

    Style : Create the loading screen and used particles.js

--- main/script.js
+++ main/script.js
@@ -1,1 +1,127 @@
 import './styles.scss';
+
+function displayParticles(speed,color){
+  particlesJS("particles-js",
+    {
+       "particles": {
+         "number": {
+           "value": 100,
+           "density": {
+             "enable": true,
+             "value_area": 1100
+           }
+         },
+         "color": {
+           "value": color
+         },
+         "shape": {
+           "type": "circle",
+           "stroke": {
+             "width": 0,
+             "color": "#000000"
+           },
+           "polygon": {
+             "nb_sides": 5
+           },
+           "image": {
+             "src": "img/github.svg",
+             "width": 100,
+             "height": 100
+           }
+         },
+         "opacity": {
+           "value": 0.5,
+           "random": false,
+           "anim": {
+             "enable": false,
+             "speed": 1,
+             "opacity_min": 0.1,
+             "sync": false
+           }
+         },
+         "size": {
+           "value": 3,
+           "random": true,
+           "anim": {
+             "enable": false,
+             "speed": 40,
+             "size_min": 0.1,
+             "sync": false
+           }
+         },
+         "line_linked": {
+           "enable": true,
+           "distance": 150,
+           "color": color,
+           "opacity": 0.4,
+           "width": 1
+         },
+         "move": {
+           "enable": true,
+           "speed":speed,
+           "direction": "none",
+           "random": false,
+           "straight": false,
+           "out_mode": "out",
+           "bounce": false,
+           "attract": {
+             "enable": false,
+             "rotateX": 600,
+             "rotateY": 1200
+           }
+         }
+       },
+       "interactivity": {
+         "detect_on": "canvas",
+         "events": {
+           "onhover": {
+             "enable": false,
+             "mode": "repulse"
+           },
+           "onclick": {
+             "enable": true,
+             "mode": "push"
+           },
+           "resize": true
+         },
+         "modes": {
+           "grab": {
+             "distance": 400,
+             "line_linked": {
+               "opacity": 1
+             }
+           },
+           "bubble": {
+             "distance": 400,
+             "size": 40,
+             "duration": 2,
+             "opacity": 8,
+             "speed": 3
+           },
+           "repulse": {
+             "distance": 200,
+             "duration": 0.4
+           },
+           "push": {
+             "particles_nb": 4
+           },
+           "remove": {
+             "particles_nb": 2
+           }
+         }
+       },
+       "retina_detect": true
+  });
+};
+
+
+displayParticles(3,document.querySelector('body').className === 'dark' ? '#EEEEEE' : '#222831');
+
+window.addEventListener('resize',()=>{
+  const bodyEl = document.querySelector('body');
+  const particlesColor = bodyEl.className === 'dark' ? '#66FCF1' : '#1f2c5c';
+  const pJS = window.pJSDom[0].pJS;
+  pJS.particles.color.value = particlesColor;
+  pJS.particles.line_linked.color = particlesColor;
+  pJS.fn.particlesRefresh();
+});
